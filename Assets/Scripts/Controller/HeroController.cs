bcd5eead30c94e237715343ec695d359b9f8dbad
lianjiusheng <lianjiusheng@126.com>
2016-10-26 17:42:01 +0800

    移动

--- Assets/Scripts/Controller/HeroController.cs
+++ Assets/Scripts/Controller/HeroController.cs
@@ -5,10 +5,13 @@ public class HeroController : MonoBehaviour {
     
     public int MaxSpeed;
 
+    public int offset;
+
     private Direct direct;
     private State state;
     public string spritName;
     private Animator animator;
+
 
     private Vector3 target;
 
@@ -47,6 +50,35 @@ public class HeroController : MonoBehaviour {
 
 
 
+
+        if (Input.GetButtonDown("Fire1")) {
+
+
+            //获取鼠标点击的屏幕左边转成世界坐标
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float v = Input.mousePosition.x;
+
+            float h = Input.mousePosition.y;
+
+            Debug.Log("MOUSE CLICK ON SCREEN(" + v + "," + h + ")");
+
+            Vector3 target = ray.origin;
+
+            target.z = 0;
+
+            Debug.Log("MOUSE CLICK ON WORLD (" + target.x + "," + target.y + ")");
+
+            if (this.state != State.STATE_MOVE)
+            {
+                //非空闲状态，先设置为空闲状态
+                animator.SetInteger("state", State.STATE_MOVE.GetHashCode());
+                this.state = State.STATE_MOVE;
+            }
+            this.target = target;
+        }
+
+
         if (this.state == State.STATE_MOVE) {
 
             //未设置目标
@@ -55,33 +87,97 @@ public class HeroController : MonoBehaviour {
             }
 
             Vector3 currentPostion = transform.position;
+            Vector3 targetPostion = target;
 
-            //以抵达目标
+            //已抵达目的地
             if (target == currentPostion) {
                 state = State.STATE_IDLE;
                 return;    
             }
 
-            Vector3 distance = target - currentPostion;
+            Vector3 dis = targetPostion - currentPostion;
 
-            
+            float currentX = currentPostion.x;
+            float currentY = currentPostion.y;
 
+            float targetX = target.x;
+            float targetY = target.y;
 
+            Direct direction = 0;
+            if (targetX < currentX)
+            {
+                //左侧
+                if (targetY == currentY)
+                {
+                    direction = Direct.DIRECT_6;
+                }
+                else if (targetY > currentY)
+                {
+                    direction = Direct.DIRECT_7;
+                }
+                else
+                {
+                    direction = Direct.DIRECT_5;
+                }
 
+            }
+            else if (targetX > currentX)
+            {
+                //右侧
+                //左侧
+                if (targetY == currentY)
+                {
+                    direction = Direct.DIRECT_2;
+                }
+                else if (targetY > currentY)
+                {
+                    direction = Direct.DIRECT_1;
+                }
+                else
+                {
+                    direction = Direct.DIRECT_3;
+                }
 
+            }
+            else{
+                 if (targetY > currentY)
+                {
+                    direction = Direct.DIRECT_8;
+                }
+                else if (targetY > currentY)
+                {
+                    direction = Direct.DIRECT_4;
+                }
+            }
 
+            if (this.direct != direction)
+            {
+                //如果需要改变方向
+                if (state != State.STATE_DIE)
+                {
+                    //非空闲状态，先设置为空闲状态
+                    animator.SetInteger("state", State.STATE_IDLE.GetHashCode());
+                    this.state = State.STATE_IDLE;
+                }
+                //改变方向
+                animator.SetInteger("direct", direction.GetHashCode());
+                this.direct = direction;
 
-            int Xspeed = 0;
-            int Yspeed = 0;
+                //改为move状态
+                animator.SetInteger("state", State.STATE_MOVE.GetHashCode());
+                this.state = State.STATE_MOVE;
 
-
-           
-            
-
+            }else {
+                //不需要改变方向
+                //移动
+                //currentPostion += dis * Time.deltaTime*MaxSpeed;
+                //transform.position=(currentPostion);
+                iTween.MoveTo(gameObject, target, 10.0f);
+            }
         }
 	}
 
-    public void moveTo(Vector2 target) {
+    public void moveTo(Vector3 target) {
         this.target = target;
 
     }
